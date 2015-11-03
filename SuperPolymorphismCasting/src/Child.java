5a6b3967214a0d84014eec4f0003bbc9051c5d59
Thomas Schwappach <Thomas.schwappach@gmail.com>
2015-11-03 14:25:01 -0700

    finished

--- SuperPolymorphismCasting/src/Child.java
+++ SuperPolymorphismCasting/src/Child.java
@@ -5,28 +5,28 @@ public class Child extends Parent
 		
 		public Child()
 			{
-				
+				super();
 			}
 		
 		public void displayChildA()
 			{
-			System.out.println("Child Method A");
+				System.out.println("Child Method A");
 			}
 
 		public void displayParentB()
 			{
-			System.out.println("Child Method B");
+				System.out.println("Child Method B");
 			}
 		
 		public void printDisplayMessages()
 			{
-			displayParentA();
-			displayParentB();
-			displayChildA();
+				displayParentA();
+				displayParentB();
+				displayChildA();
 			}
 		
 		public void printNumber_ChildClass()
 			{
-			System.out.println("The number is " + num);
+				System.out.println("The number is " + num);
 			}
 	}
--- SuperPolymorphismCasting/src/Parent.java
+++ SuperPolymorphismCasting/src/Parent.java
@@ -1,30 +1,30 @@
 
 public class Parent
 	{
-	protected int num = 10;
+		protected int num = 10;
+		
+		public Parent()
+			{
+				System.out.println("Hello, you've reached the default constructor.");
+			}
 	
-//	public Parent()
-//		{
-//		System.out.println("Hello, you've reached the default constructor.");
-//		}
-//	
-//	public Parent(String message)
-//		{
-//		System.out.println("You managed to print " + message);
-//		}
+		public Parent(String message)
+			{
+				System.out.println("You managed to print " + message);
+			}
+		
+		public void displayParentA()
+			{
+				System.out.println("Parent Method A");
+			}
 	
-	public void displayParentA()
-		{
-		System.out.println("Parent Method A");
-		}
-
-	public void displayParentB()
-		{
-		System.out.println("Parent Method B");
-		}
-	
-	public void printNumber_ParentClass()
-		{
-		System.out.println("The number is " + num);
-		}
+		public void displayParentB()
+			{
+				System.out.println("Parent Method B");
+			}
+		
+		public void printNumber_ParentClass()
+			{
+				System.out.println("The number is " + num);
+			}
 	}
